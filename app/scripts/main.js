e54315e8983c3514b539b942dd53794a3945161a
Lucas Pirola <lucaspirola@gmail.com>
2014-04-11 19:18:48 -0300

    adicionado js base para foundation; html topo inserido, sem css;

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,14 @@
 require.config({
     paths: {
-        jquery: '../bower_components/jquery/jquery'
+        jquery     : '../bower_components/jquery/dist/jquery',
+        foundation : '../bower_components/foundation/js/foundation'
 }
 });
 
-require(['app', 'jquery'], function (app, $) {
+require(['app', 'jquery', 'foundation'], function (app, jQuery) {
     'use strict';
     // use app here
     console.log(app);
-    console.log('Running jQuery %s', $().jquery);
+    console.log('Running jQuery %s', jQuery().jquery);
+    $(document).foundation();
 });
